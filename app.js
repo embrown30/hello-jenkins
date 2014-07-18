b79a2233ea62e20cacdac51081198a63b72a7b40
embrown30 <em.brown30@gmail.com>
2014-07-17 22:13:56 -0400

    Switch to hello yall

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res) { res.send('hello jenkins'); });
+app.get('/', function (req, res) { res.send('hello yall'); });
 
 app.listen(process.env.PORT || 5000);
 
